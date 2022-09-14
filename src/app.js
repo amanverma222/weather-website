d912da05ca0a9a33c0669f416cb9c67294f9e912
aman verma <vaman8349@gmail.com>
2022-09-14 20:25:45 +0530

    New update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.port || 3000
 
 //Define Paths for Express config
 const publicpathdirectory = path.join(__dirname, '../public')
